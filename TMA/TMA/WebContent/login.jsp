3c3bcb0cfb1281ff8499703a76db9376974d6dda
RaonePatel <raonepatel007@gmail.com>
2016-10-14 19:48:44 +0530

    14/10/2014 07:47 PM

---
+++ TMA/TMA/WebContent/login.jsp
@@ -0,0 +1,15 @@
+<html>
+<head>
+<meta content="text/html; charset=ISO-8859-1"
+http-equiv="content-type">
+<title></title>
+</head>
+<body>
+<form method="post" action="Login" name="index">
+Email ID : <input name="emailId" type="text" value="khush@gmail.com"> <br>
+Password : <input name="password" type="password" value="khushpass"><br>
+<input type="submit" value="Submit">
+</form>
+</body>
+</html>
+
---
+++ TMA/TMA/src/Login.java
@@ -0,0 +1,72 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Login extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public Login() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+	}
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter out= response.getWriter();
+		
+		
+		
+		try
+		{
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/travel_mng","root","root");
+			Statement stmt=con.createStatement();			
+			
+			String emailId= request.getParameter("emailId");
+			String password = request.getParameter("password");
+			
+			String query = "select * from userLogin where emailId='" + emailId + "' and password='" + password +"'";
+			
+			System.out.println("sdadadads");
+			//stmt.executeUpdate(query);
+			
+			stmt.executeQuery(query);
+			
+			RequestDispatcher rd = request.getRequestDispatcher("Welcome.jsp");
+		    rd.forward(request, response); 
+			
+			
+			
+			System.out.println("done");
+			
+			
+		}
+		
+		catch(SQLException e)
+		{
+			e.printStackTrace();	
+		} 
+		catch (ClassNotFoundException e) 
+		{
+			e.printStackTrace();
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+
+}
